eb2b325da0ce005488155b47b18b9893e94dc6b3
Carlos Pariona <infocat2.0@gmail.com>
2023-08-08 18:34:12 -0500

    4.51

---
+++ Extras/parche_20230807.sql
@@ -0,0 +1,2 @@
+INSERT INTO `precios` (`id`, `descripcion`, `nuevos`, `continuos`, `idClasificacion`, `activo`) VALUES (NULL, 'Otros Certificados', '60', '60', '3', '1');
+UPDATE `precios` SET `descripcion` = 'Reevaluación por informe verbal' WHERE `precios`.`id` = 18;
